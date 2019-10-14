1e4156a86c02ce386c5ea7462d22772ea28e771b
Ross Hudson <hello@rosshudson.co.uk>
2019-10-14 15:44:51 +0100

    Reworked modes as scales.

--- docs/js/soundscape/tunings.js
+++ docs/js/soundscape/tunings.js
@@ -38,18 +38,13 @@ tunings.just = {};
 tunings.just.notes = [];
 tunings.just.set = function() {};
 
-// Modes.
+// Scales.
 
-tunings.modes = {
-  ionian: [2, 2, 1, 2, 2, 2, 1],
-  dorian: [2, 1, 2, 2, 2, 1, 2],
-  phrygian: [1, 2, 2, 2, 1, 2, 2],
-  lydian: [2, 2, 2, 1, 2, 2, 1],
-  mixolydian: [2, 2, 1, 2, 2, 1, 2],
-  aeolian: [2, 1, 2, 2, 1, 2, 2],
-  locrian: [1, 2, 2, 1, 2, 2, 2],
-  majorPentatonic: [2, 2, 2, 3, 2],
-  minorPentatonic: [3, 2, 2, 3],
+tunings.scales = {
+  majorHeptatonic: [2, 2, 1, 2, 2, 2, 1],
+  minorHeptatonic: [2, 1, 2, 2, 1, 2, 2],
+  majorPentatonic: [2, 2, 3, 2, 3],
+  minorPentatonic: [3, 2, 2, 3, 2],
   wholeTone: [2, 2, 2, 2, 2, 2]
 };
 
@@ -58,13 +53,13 @@ tunings.keys.set = function() {
   tunings[tunings.settings.tuningSystem].notes.forEach(function(note, i) {
     var noteName = Object.keys(note)[0];
     var frequency = note[Object.keys(note)[0]];
-    Object.keys(tunings.modes).forEach(function(modeName, index) {
+    Object.keys(tunings.scales).forEach(function(modeName, index) {
       var keyName = noteName + "_" + modeName;
 
       var notes = [];
       notes.push(frequency);
 
-      var intervals = tunings.modes[modeName];
+      var intervals = tunings.scales[modeName];
       var currentFreq = i;
       intervals.forEach(function(interval, index) {
         currentFreq += interval;
